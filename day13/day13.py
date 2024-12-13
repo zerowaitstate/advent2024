165487e351af16c357ae9d49e9b9662bb0572e4c
zerowaitstate <danielwaites@proton.me>
2024-12-13 11:49:09 -0600

    day13 solved part2

--- day13/day13.py
+++ day13/day13.py
@@ -20,10 +20,9 @@ def verify(m, a, b):
     y0 = m[1][2]
     return ax * a + bx * b == x0 and ay * a + by * b == y0
 
-if __name__ == '__main__':
+def part1():
     a_cost = 3
     b_cost = 1
-    max_err = 0.00000000000000000001
     token_acc = 0
     pat = re.compile(r'Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)', re.MULTILINE)
     with open('day13.txt', 'r') as f:
@@ -36,4 +35,27 @@ if __name__ == '__main__':
             b_press = round(b)
             if verify(m, a_press, b_press):
                 token_acc += a_press * a_cost + b_press * b_cost
-    print(token_acc)
+    return token_acc
+
+def part2():
+    a_cost = 3
+    b_cost = 1
+    token_acc = 0
+    pat = re.compile(r'Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)', re.MULTILINE)
+    with open('day13.txt', 'r') as f:
+        data = f.read()
+        for m in re.finditer(pat, data):
+            m = [[int(m.group(1)), int(m.group(3)), int(m.group(5))+10000000000000],
+                 [int(m.group(2)), int(m.group(4)), int(m.group(6))+10000000000000]]
+            a, b = solve_2x2(m)
+            a_press = round(a)
+            b_press = round(b)
+            if verify(m, a_press, b_press):
+                token_acc += a_press * a_cost + b_press * b_cost
+    return token_acc
+
+
+if __name__ == '__main__':
+    print('part1', part1())
+    print('part2', part2())
+
